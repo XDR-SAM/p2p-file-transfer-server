03d064a40edea3df5a7313066df25737819906d0
XDR-SAM <122539035+XDR-SAM@users.noreply.github.com>
2025-02-11 11:27:21 +0600

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
-
+app.use(express.static("public"));
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () =>
   console.log(`Signaling server running on port ${PORT}`)
